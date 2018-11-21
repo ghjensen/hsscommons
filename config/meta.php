157ccc86eda95bfef75e21734662c0a6067554a7
ijia <ivy.jia@canarie.ca>
2018-11-21 09:37:44 -0500

    remove files under config folder

--- config/meta.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	'MetaAuthor' => '1',
-	'MetaTitle' => '1',
-	'MetaDesc' => 'Canadian HSS Commons - Community for Scientific and Educational Collaboration',
-	'MetaKeys' => 'hsscommons,HSS Commons,',
-	'MetaRights' => '',
-	'MetaVersion' => '0',
-);
--- config/rate_limit.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-	'short' => array("period" => "1", "limit" => "120"),
-	'long' => array("period" => "1440", "limit" => "10000"),
-);
